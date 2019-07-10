039dfed7a9a17a83a9cc41e201f62f3b62061d82
Oleksii <mysterious240@gmail.com>
2019-07-11 01:48:22 +0300

    fff

---
+++ src/components/common/index.js
@@ -0,0 +1,8 @@
+export { default as Button, TypesButton } from './Button';
+export { default as TextField } from './TextField';
+export { default as Select } from './Select';
+export { default as Headline } from './Headline';
+export { default as Checkbox } from './Checkbox';
+export { default as Input } from './Input';
+export { default as RadioButton } from './RadioButton';
+
